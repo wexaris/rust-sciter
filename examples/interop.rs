d286d9d0c26ab56a30dd30aab679521f03fcae2d
pravic <ehysta@gmail.com>
2016-03-25 18:35:55 +0300

    example: interop - show new script dispatching feature

--- examples/interop.rs
+++ examples/interop.rs
@@ -1,6 +1,7 @@
 //! Sciter interop with native code and vice versa.
 
 #![allow(unused_variables)]
+#![allow(non_snake_case)]
 
 #[macro_use]
 extern crate sciter;
@@ -51,7 +52,50 @@ impl EventHandler {
 
 		return Some(Value::from(true));
 	}
+
+	fn NativeCall(&mut self, arg: String) -> Value {
+		return Value::from(format!("Rust window ({})", arg));
+	}
+
+	fn GetNativeApi(&mut self, ) -> Value {
+
+		fn on_add(args: &[Value]) -> Value {
+			let ints = args.iter().map(|ref x| x.to_int().unwrap());
+			// let sum: i32 = ints.sum();	// error: issue #27739
+			let sum: i32 = ints.fold(0, |sum, x| sum + x);
+			return Value::from(sum);
+		}
+
+		fn on_sub(args: &[Value]) -> Value {
+			if args.len() != 2 || args.iter().any(|x| !x.is_int()) {
+				return Value::error("sub requires 2 integer arguments!");
+			}
+			let ints: Vec<_> = args.iter().map(|ref x| x.to_int().unwrap()).collect();
+			let (a,b) = (ints[0], ints[1]);
+			return Value::from(a - b);
+		}
+
+		let on_mul = |args: &[Value]|  -> Value {
+			let prod = args.iter().map(|ref x| x.to_int().unwrap()).fold(1, |total, x| total * x);
+			Value::from(prod)
+		};
+
+		let mut api = Value::new();
+
+		api.set_item(Value::from("add"), Value::from(on_add));
+		api.set_item(Value::from("sub"), Value::from(on_sub));
+		api.set_item(Value::from("mul"), Value::from(on_mul));
+
+		println!("returning {:?}", api);
+		return api;
+	}
+
+	fn calc_sum(&mut self, a: i32, b: i32) -> i32 {
+		a + b
+	}
+
 }
+
 
 impl sciter::EventHandler for EventHandler {
 
@@ -59,49 +103,26 @@ impl sciter::EventHandler for EventHandler {
 		self.root = Some(Element::from(root));
 	}
 
+	dispatch_script_call! {
+
+		fn NativeCall(String);
+
+		fn GetNativeApi();
+
+		fn calc_sum(i32, i32);
+	}
+
 	fn on_script_call(&mut self, root: HELEMENT, name: &str, argv: &[Value]) -> Option<Value> {
+
 		let args = argv.iter().map(|ref x| format!("{}", &x)).collect::<Vec<String>>().join(", ");
 		println!("script->native: {}({}), root {:?}", name, args, root);
 
+		let handled = self.dispatch_script_call(root, name, argv);
+		if handled.is_some() {
+			return handled;
+		}
+
 		match name {
-			"NativeCall" => {
-				return Some(Value::from(format!("Rust window ({})", argv[0])));
-			},
-
-			"GetNativeApi" => {
-
-				fn on_add(args: &[Value]) -> Value {
-					let ints = args.iter().map(|ref x| x.to_int().unwrap());
-					// let sum: i32 = ints.sum();	// error: issue #27739
-					let sum: i32 = ints.fold(0, |sum, x| sum + x);
-					return Value::from(sum);
-				}
-
-				fn on_sub(args: &[Value]) -> Value {
-					if args.len() != 2 || args.iter().any(|x| !x.is_int()) {
-						return Value::error("sub requires 2 integer arguments!");
-					}
-					let ints: Vec<_> = args.iter().map(|ref x| x.to_int().unwrap()).collect();
-					let (a,b) = (ints[0], ints[1]);
-					return Value::from(a - b);
-				}
-
-				let on_mul = |args: &[Value]|  -> Value {
-					let prod = args.iter().map(|ref x| x.to_int().unwrap()).fold(1, |total, x| total * x);
-					Value::from(prod)
-				};
-
-				let mut api = Value::new();
-
-				api.set_item(Value::from("add"), Value::from(on_add));
-				api.set_item(Value::from("sub"), Value::from(on_sub));
-				api.set_item(Value::from("mul"), Value::from(on_mul));
-
-				println!("returning {:?}", api);
-
-				return Some(api);
-
-			},
 
 			"ScriptCallTest" => {
 				return self.script_call_test(argv, Element::from(root));
