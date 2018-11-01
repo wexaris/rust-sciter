d9d5a4773d0775367cbf28760b08090610b40d46
pravic <ehysta@gmail.com>
2018-11-01 15:12:13 +0300

    example: Use "dynamic" too.
    refs 25eb781027

--- examples/first.rs
+++ examples/first.rs
@@ -4,7 +4,7 @@ extern crate sciter;
 
 fn main() {
   // can be called as `examples/first ~/lib/libsciter.so`
-  if cfg!(feature = "shared") {
+  if cfg!(feature = "dynamic") {
     if let Some(arg) = std::env::args().nth(1) {
       if let Err(_) = sciter::set_options(sciter::RuntimeOptions::LibraryPath(&arg)) {
         panic!("Invalid library path specified.");
